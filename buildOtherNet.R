0b422d56f47215b51e90da5a0ae8fd4a2679afa5
Ben Logsdon <aeberhart@gmail.com>
2015-04-02 13:44:04 -0700

    function to do aracne and wgcna

---
+++ buildOtherNet.R
@@ -0,0 +1,11 @@
+require(metaNet)
+
+fileName <- as.character(commandArgs(TRUE)[[1]])
+pathv <- as.character(commandArgs(TRUE)[[3]])
+functionName <- as.character(commandArgs(TRUE)[[4]])
+data <- data.matrix(read.csv(fileName,row.names=1))
+if(functionName=='aracne'){
+  installAracne()
+}
+do.call(what=functionName,args=list(data=data,path=pathv))
+#sparrowMPI(data,nodes,pathv)

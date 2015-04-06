3cf36b7d6e25ef46812b0bdda0e9518b94ed163d
Ben Logsdon <aeberhart@gmail.com>
2015-04-06 10:17:58 -0700

    debugging code

--- buildMpiNet.R
+++ buildMpiNet.R
@@ -4,6 +4,8 @@ fileName <- as.character(commandArgs(TRUE)[[1]])
 nodes <- as.numeric(commandArgs(TRUE)[[2]])
 pathv <- as.character(commandArgs(TRUE)[[3]])
 functionName <- as.character(commandArgs(TRUE)[[4]])
+cat(fileName,nodes,pathv,functionName,'\n')
+
 data <- data.matrix(read.csv(fileName,row.names=1))
 do.call(what=functionName,args=list(data=data,nodes=nodes,pathv=pathv))
 #sparrowMPI(data,nodes,pathv)

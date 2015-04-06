d69566e31d4c8c90e822783fb29047ecd7c30718
Ben Logsdon <aeberhart@gmail.com>
2015-04-06 11:09:22 -0700

    fix bugs

--- buildOtherNet.R
+++ buildOtherNet.R
@@ -1,8 +1,8 @@
 require(metaNet)
 
 fileName <- as.character(commandArgs(TRUE)[[1]])
-pathv <- as.character(commandArgs(TRUE)[[3]])
-functionName <- as.character(commandArgs(TRUE)[[4]])
+pathv <- as.character(commandArgs(TRUE)[[2]])
+functionName <- as.character(commandArgs(TRUE)[[3]])
 data <- data.matrix(read.csv(fileName,row.names=1))
 if(functionName=='aracne'){
   installAracne()

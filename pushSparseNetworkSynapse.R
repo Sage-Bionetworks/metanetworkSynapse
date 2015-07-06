3cb2c07e806b02c7ddb8f31e151c80f91b3e7601
Ben Logsdon <aeberhart@gmail.com>
2015-07-06 10:22:05 -0700

    updated sparse network

--- pushSparseNetworkSynapse.R
+++ pushSparseNetworkSynapse.R
@@ -3,6 +3,8 @@
 require(synapseClient)
 synapseLogin()
 require(metaNet)
+require(dplyr)
+
 #syn id of project to post networks to
 #file with syn ids of data used to run network analysis
 #file with code urls of code used to run network analysis
@@ -22,23 +24,79 @@ tissueType <- as.character(commandArgs(TRUE)[[6]])
 disease <- as.character(commandArgs(TRUE)[[7]])
 organism <- as.character(commandArgs(TRUE)[[8]])
 edgeType <- as.character(commandArgs(TRUE)[[9]])
+#sparsitySyn <- as.character(commandArgs(TRUE)[[10]])
+#sparObj <- synGet(sparsitySyn)
+#spar <- read.csv(sparObj@filePath,stringsAsFactors=F,row.names=1)
 
 
 #load data
 #load(paste0('result_',method,'.rda'))
-require(Matrix)
+
+#load sparsities
+sparsity <- data.frame(t(read.csv('sparsity.csv',header=F,row.names=1)))
+
+if(method=='sparrow1'){
+  load('result_sparrowZ.rda')
+} else if (method=='sparrow2'){
+  load('result_sparrow2Z.rda')
+}else if (method=='aracne'){
+  load('result_aracneFull.rda')
+}else if (method=='correlation'){
+  load('result_correlation.rda')
+}else if (method=='lassoCV1se'){
+  load('result_lassoCV1se.rda')
+}else if (method=='lassoCVmin'){
+  load('result_lassoCVmin.rda')
+}else if (method=='lassoAIC'){
+  load('result_lassoAIC.rda')
+}else if (method=='lassoBIC'){
+  load('result_lassoBIC.rda')
+}else if (method=='ridgeCV1se'){
+  load('result_ridgeCV1se.rda')
+}else if (method=='ridgeCVmin'){
+  load('result_ridgeCVmin.rda')
+}else if (method=='ridgeBIC'){
+  load('result_ridgeBIC.rda')
+}else if (method=='ridgeAIC'){
+  load('result_ridgeAIC.rda')
+}else if (method=='tigress'){
+  load('result_tigress.rda')
+}else if (method=='genie3'){
+  load('result_genie3.rda')
+}
+
+#enumurate methods
+
+#sparsity <- read.csv('sparsity.csv',stringsAsFactors=F,row.names=1,header=F)
+#colnames(network) <- rownames(network)
+##CHANGE FILE NAME
 file <- paste0(method,'_',disease,'_',normalization,'.csv')
-fileS <- paste0(method,'_',disease,'_',normalization,'.rda')
-network <- data.matrix(read.csv(file=file,row.names=1))
-network <- Matrix(network,sparse=TRUE)
 
 
-#save file
-save(network,file=fileS)
-#Sys.sleep(5)
+
+library(Matrix)
+  cat('changing to matrix\n')
+  network  <- network %>% as.matrix
+  diag(network) <- 0
+  cat('symmetrisizing\n')
+  network <- network %>% symmetrisize
+  #multiNetwork <- sparsity %>% lapply(arbitrarySparsity,network)
+  #multiNetwork$scaleFreeNetwork <- applyScaleFree(network)
+  nEdgesScaleFreeNetwork <- NA
+  #cat('make scale free\n')
+  #try(nEdgesScaleFreeNetwork <- applyScaleFree(network),silent=T)
+  if(!is.na(nEdgesScaleFreeNetwork)){
+    cat(paste0(method,'ScaleFree,',nEdgesScaleFreeNetwork,'\n'),append = T,file = 'sparsity.csv')
+  }
+  cat('apply ranked edge list\n')
+  #edgeList <- rankedEdgeList(network,symmetric = TRUE)
+
+
+  
+#WRITE TO RDA
 
 #make synapse object
-synNet <- File(fileS,parentId=parentId)
+synNet <- File(file,parentId=parentId)
 
 #definte annotation
 networkAnnotation <- list(
@@ -46,11 +104,9 @@ networkAnnotation <- list(
   disease = disease,
   normalization = normalization,
   method = method,
-  fileType = 'rda',
+  fileType = 'csv',
   organism = organism,
-  dataType = 'metaData',
-  edgeType = edgeType,
-  matrixType = 'sparse'
+  dataType = 'metaData'
 )
 
 #set annotations

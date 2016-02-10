d489d2ba1880b34160cc0949434b51e928387d6c
Benjamin Logsdon <aeberhart@gmail.com>
2016-02-10 10:45:15 -0800

    migrate csv

--- buildNet.sh
+++ buildNet.sh
@@ -6,63 +6,63 @@
 #if sparrow:
 if [ $sparrowZ -eq "1" ]; then
   mpirun -np 1 Rscript $pathv/buildMpiNet.R $dataFile $((numberCore-1)) $pathv "sparrowZ" $outputpath
-  aws s3 mv $outputpath/result_sparrowZ.rda $s3
+  aws s3 mv $outputpath/sparrowZNetwork.csv $s3
   #run script linking to synapse
   #add annotations, provenance, etc...
 fi
 
 if [ $mrnet -eq "1" ]; then
   Rscript $pathv/buildOtherNet.R $dataFile $pathv "mrnetWrapper" "NULL" $outputpath
-  aws s3 mv $outputpath/result_mrnet.rda $s3
+  aws s3 mv $outputpath/mrnetNetwork.csv $s3
 fi
 
 if [ $c3net -eq "1" ]; then
   Rscript $pathv/buildOtherNet.R $dataFile $pathv "c3netWrapper" "NULL" $outputpath
-  aws s3 mv $outputpath/result_c3net.rda $s3
+  aws s3 mv $outputpath/c3netNetwork.csv $s3
 fi
 
 #if lasso
 if [ $lassoCV1se -eq "1" ]; then
   mpirun -np 1 Rscript $pathv/buildMpiNet.R $dataFile $((numberCore-1)) $pathv "lassoCV1se" $outputpath
-  aws s3 mv $outputpath/result_lassoCV1se.rda $s3
+  aws s3 mv $outputpath/lassoCV1seNetwork.csv $s3
 fi
 
 #if ridge
 if [ $ridgeCV1se -eq "1" ]; then
   mpirun -np 1 Rscript $pathv/buildMpiNet.R $dataFile $((numberCore-1)) $pathv "ridgeCV1se" $outputpath
-  aws s3 mv $outputpath/result_ridgeCV1se.rda $s3
+  aws s3 mv $outputpath/ridgeCV1seNetwork.csv $s3
 fi
 
 #if genie3
 if [ $genie3 -eq "1" ]; then
   mpirun -np 1 Rscript $pathv/buildMpiNet.R $dataFile $((numberCore-1)) $pathv "genie3" $outputpath
-  aws s3 mv $outputpath/result_genie3.rda $s3
+  aws s3 mv $outputpath/genie3Network.csv $s3
 fi
 
 #if tigress
 if [ $tigress -eq "1" ]; then
   mpirun -np 1 Rscript $pathv/buildMpiNet.R $dataFile $((numberCore-1)) $pathv "tigress" $outputpath
-  aws s3 mv $outputpath/result_tigress.rda $s3
+  aws s3 mv $outputpath/tigressNetwork.csv $s3
 fi
 
 #if aracne
 if [ $aracne -eq "1" ]; then
   Rscript $pathv/buildOtherNet.R $dataFile $pathv "aracne" "NULL" $outputpath
-  aws s3 mv $outputpath/result_aracneFull.rda $s3
+  aws s3 mv $outputpath/aracneNetwork.csv $s3
 fi
 
 if [ $correlation -eq "1" ]; then
   Rscript $pathv/buildOtherNet.R $dataFile $pathv "correlation" "NULL" $outputpath
-  aws s3 mv $outputpath/result_correlation.rda $s3
+  aws s3 mv $outputpath/correlationNetwork.csv $s3
 fi
 
 #if wgcna
 if [ $wgcnaST -eq "1" ]; then
   Rscript $pathv/buildOtherNet.R $dataFile $pathv "wgcnaSoftThreshold" "NULL" $outputpath
-  aws s3 mv $outputpath/result_wgcnaST.rda $s3
+  aws s3 mv $outputpath/wgcnaSoftThresholdNetwork.csv $s3
 fi
 
 if [ $wgcnaTOM -eq "1" ]; then
   Rscript $pathv/buildOtherNet.R $dataFile $pathv "wgcnaTOM" "NULL" $outputpath
-  aws s3 mv $outputpath/result_wgcnaTOM.rda $s3
+  aws s3 mv $outputpath/wgcnaTopologicalOverlapMatrixNetwork.csv $s3
 fi

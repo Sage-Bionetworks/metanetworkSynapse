df5a95c07a40ab48a2c53dbf5e748b36cc70d316
Benjamin Logsdon <aeberhart@gmail.com>
2016-03-03 14:47:22 -0800

    consensus building scripts

---
+++ buildConsensus.R
@@ -0,0 +1,39 @@
+fileName <- as.character(commandArgs(TRUE)[[1]])
+outputpath <- as.character(commandArgs(TRUE)[[2]])
+networkFolderId <- as.character(commandArgs(TRUE)[[3]])
+provenanceFile <- as.character(commandArgs(TRUE)[[4]])
+
+buildConsensus = function(fileName,outputpath,networkFolderId,provenanceFile){
+  library(synapseClient)
+  library(metanetwork)
+  synapseLogin()
+
+  #get all networks from Synapse
+  foo <- synQuery(paste0('select name,id from file where parentId==\',networkFolderId,\''))
+  bar <- lapply(foo$file.id,synGet,downloadLocation=outputpath)
+
+  #update provenanceFile
+  provenance <- read.csv(provenanceFile,stringsAsFactors=F,row.names=F)
+  provenance <- rbind(provenance,cbind(foo$file.id,rep(FALSE,nrow(foo))))
+  write.csv(provenance,paste0(outputpath,'rankConsensusProvenanceFile.txt'),quote=F,row.names=F)
+
+  #load networks into R session
+
+  loadNetwork <- function(file){
+    library(data.table)
+    sparrowNetwork <- data.table::fread(file,stringsAsFactors=FALSE,data.table=F)
+    rownames(sparrowNetwork) <- sparrowNetwork$V1
+    sparrowNetwork <- sparrowNetwork[,-1]
+    gc()
+    return(sparrowNetwork)
+  }
+
+  networkFiles <- sapply(bar,function(x){return(x@filePath)})
+  networks <- lapply(networkFiles,loadNetwork)
+
+  #build rank consensus
+  rankConsensusNetwork <- metanetwork::rankConsensus(networks)
+
+  write.csv(network,file=paste0(outputpath,'rankConsensusNetwork.csv'),quote=F)
+
+}

713bf4fed38afd542d9b64ddbf8a9883cc51bec3
Cloud User <centos@ip-172-31-21-32.us-west-2.compute.internal>
2016-09-02 23:05:06 +0000

    refactored variables specifying input/output file paths

---
+++ config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#full s3 path where networks will go
+s3="s3://metanetworksynpasetestbucket/testNetwork/"
+
+#location of data file
+dataFile="/shared/testNetwork/testData.csv"
+
+#location of metanetwork synapse scripts
+pathv="/shared/metanetworkSynapse/"
+
+#output path for temporary result file prior to pushing to s3/synapse
+outputpath="/shared/testNetwork/"
+
+#path within s3
+s3b="testNetwork"
+
+#id of folder on Synapse that files will go to
+parentId="syn7209613"
+
+#id of folder with networks to combine (for buildConsensus)
+networkFolderId="syn7209613"
--- testNetworkBuildConsensus.sh
+++ testNetworkBuildConsensus.sh
@@ -1,40 +1,24 @@
 #!/bin/sh
+
+. ./config.sh
+
 #number of cores to reserve for job
-nthreads=1
-
-#full s3 path where networks will go
-s3="s3://metanetworks/testNetwork/"
-
-#location of data file
-dataFile="/shared/testNetwork/testData.csv"
-
-#location of metanetwork synapse scripts
-pathv="/shared/metanetworkSynapse/"
-
-#output path for temporary result file prior to pushing to s3/synapse
-outputpath="/local/testNetwork/"
-
-#path within s3
-s3b="testNetwork"
-
-#id of folder with networks to combine
-networkFolderId="syn5706584"
-
-#id of folder on Synapse that file will go to
-parentId="syn5646308"
+nthreads=8
 
 #path to csv file with annotations to add to file on Synapse
-annotationFile="/shared/testNetwork/annoFile.txt"
+annotationFile="$outputpath/buildConsensusAnnoFile.txt"
 
-provenanceFile="/shared/testNetwork/provenanceFile.txt"
+provenanceFile="$outputpath/buildConsensusProvenanceFile.txt"
 
 #path to error output
-errorOutput="/shared/testNetwork/Aggregationerror.txt"
+errorOutput="$outputpath/Aggregationerror.txt"
 
 #path to out output
-outOutput="/shared/testNetwork/Aggregationout.txt"
+outOutput="$outputpath/Aggregationout.txt"
 
 #job script name
-jobname="testNetworkaggregation"
+jobname="networkAggregation"
 
-qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile,networkFolderId=$networkFolderId -pe orte $nthreads -S /bin/bash -V -cwd -N $jobname -e $errorOutput -o $outOutput $pathv/buildConsensus.sh
+qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile,networkFolderId=$networkFolderId -pe mpi $nthreads -S /bin/bash -V -cwd -N $jobname -e $errorOutput -o $outOutput $pathv/buildConsensus.sh
+
+#s3=$s3 dataFile=$dataFile pathv=$pathv outputpath=$outputpath s3b=$s3b parentId=$parentId annotationFile=$annotationFile provenanceFile=$provenanceFile networkFolderId=$networkFolderId $pathv/pushNetS3.sh

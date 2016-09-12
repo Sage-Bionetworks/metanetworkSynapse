7704f9fd3503535bdffa43655ffd4c3f0e3464b8
Phil Snyder <phil.snyder@sagebase.org>
2016-09-12 22:36:35 +0000

    split up buildConsensus scripts

---
+++ pushScripts/rankConsensus.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+aws s3 mv $outputpath/rankConsensusNetwork.csv $s3
+provenanceFileUpdated=$outputpath/rankConsensusProvenanceFile.txt
+Rscript $pathv/s3LinkToSynapse.R $s3b/rankConsensusNetwork.csv $outputpath/rankconsensustempmd5.out $parentId $annotationFile $provenanceFileUpdated "rankConsensus"
+
--- testNetworkBuildConsensus.sh
+++ testNetworkBuildConsensus.sh
@@ -8,14 +8,16 @@ pathv="/shared/metanetworkSynapse/"
 #number of cores to reserve for job
 nthreads=8
 
+#number of cores to reserve for pushing networks to S3/Synapse
+nthreadsPush=1
+
 #path to csv file with annotations to add to file on Synapse
 annotationFile="$outputpath/buildConsensusAnnoFile.txt"
 
 provenanceFile="$outputpath/buildConsensusProvenanceFile.txt"
 
-#job script name
-jobname="networkAggregation"
+qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile,networkFolderId=$networkFolderId -pe mpi $nthreads -S /bin/bash -V -cwd -N "buildConsensus" -e $errorOutput/errorLogs/buildConsensuserror.txt -o $outOutput/outLogs/buildConsensusout.txt $pathv/networkScripts/buildConsensus.sh
 
-qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile,networkFolderId=$networkFolderId -pe mpi $nthreads -S /bin/bash -V -cwd -N $jobname -e $errorOutput/Aggregationerror.txt -o $outOutput/Aggregationout.txt $pathv/buildConsensus.sh
+#qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile,networkFolderId=$networkFolderId,bucket=$bucket -hold_jid = "buildConsensus" -pe mpi $nthreadsPush -S /bin/bash -V -cwd -N "push-rankConsensus" -e $errorOutput/errorLogs/pushrankConsensuserror.txt -o $outOutput/outLogs/pushrankConsensusout.txt $pathv/pushScripts/rankConsensus.sh
 
-#s3=$s3 dataFile=$dataFile pathv=$pathv outputpath=$outputpath s3b=$s3b parentId=$parentId annotationFile=$annotationFile provenanceFile=$provenanceFile networkFolderId=$networkFolderId bucket=$bucket $pathv/pushNetS3.sh
+#qsub -v s3=$s3,dataFile=$dataFile,pathv=$pathv,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile,networkFolderId=$networkFolderId,bucket=$bucket -hold_jid "buildConsensus" -pe mpi $nthreadsPush -S /bin/bash -V -cwd -N "push-bic" -e $errorOutput/errorLogs/pushBICerror.txt -o $outOutput/outLogs/pushBICout.txt $pathv/pushScripts/bic.sh

ff3af964be9fb346ddd49aac42bf0f331138b643
Phil Snyder <subfivemile@gmail.com>
2016-09-13 10:53:19 -0700

    run remote script in background

--- localhost.sh
+++ localhost.sh
@@ -9,4 +9,6 @@ ssh -i $key centos@$address 'mkdir /shared/.aws'
 scp -i $key ~/.aws/credentials centos@$address:/shared/.aws/
 
 # run setup script on remote and add PS1 and AWS_CONFIG_FILE environment vars to .bashrc
-ssh -i $key centos@$address 'sh ~/remote.sh ${synId}'
+ssh -i $key centos@$address
+ssh -n -f -i $key centos@$address "sh -c 'nohup sh ~/remote.sh ${synId} > /dev/null 2>&1 &'"
+

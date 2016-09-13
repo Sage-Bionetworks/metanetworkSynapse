6581a5873e451bd17f41cbef4b5e58bc18843547
Phil Snyder <subfivemile@gmail.com>
2016-09-13 12:02:54 -0700

    new functionality: copy over metanetworkSynapse from localhost

--- localhost.sh
+++ localhost.sh
@@ -5,6 +5,8 @@ synId=$3
 
 # move over essentials
 scp -i $key ./remote.sh ~/.synapseConfig centos@$address:~/
+ssh -i $key centos@$address 'mkdir /shared/metanetworkSynapse'
+scp -i $key centos@$address . centos@$address:/shared/metanetworkSynapse/
 ssh -i $key centos@$address 'mkdir /shared/.aws'
 scp -i $key ~/.aws/credentials centos@$address:/shared/.aws/
 
--- remote.sh
+++ remote.sh
@@ -11,7 +11,6 @@ module load openmpi-x86_64
 Rscript -e 'install.packages("Rmpi", configure.args = paste("--with-Rmpi-include=/usr/include/openmpi-x86_64","--with-Rmpi-libpath=/usr/lib64/openmpi/lib","--with-Rmpi-type=OPENMPI"))'
 sh ~/rpackages_provisioner.sh
 Rscript -e 'install.packages(c("bit64", "parmigene", "c3net", "ROCR", "Matrix", "glasso", "utility"))'
-git clone https://github.com/philerooski/metanetworkSynapse /shared/metanetworkSynapse
 
 # test network
 mkdir /shared/testNetwork && mkdir /shared/testNetwork/errorLogs && mkdir /shared/testNetwork/outLogs && mkdir /shared/testNetwork/outLogs/push && mkdir /shared/testNetwork/errorLogs/push
@@ -21,4 +20,4 @@ echo "hello,goodbye" > /shared/testNetwork/annoFile.txt
 
 # actual network
 mkdir /shared/network && mkdir /shared/network/errorLogs && mkdir /shared/network/outLogs && mkdir /shared/network/errorLogs/push && mkdir /shared/network/outLogs/push
-Rscript -e 'library(synapseClient); synapseLogin(); synGet("$synId", downloadLocation="/shared/network/")'
+Rscript -e "library(synapseClient); synapseLogin(); synGet('$synId', downloadLocation='/shared/network/')"

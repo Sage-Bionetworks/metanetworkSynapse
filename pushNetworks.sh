ce0925eab1f64edc238ac3aff42f0845b22e55d3
Cloud User <centos@ip-172-31-15-217.ec2.internal>
2016-09-26 23:00:59 +0000

    include lassoCVMin

--- pushNetworks.sh
+++ pushNetworks.sh
@@ -5,7 +5,7 @@ pathv="/shared/metanetworkSynapse/"
 
 . $pathv/config.sh
 
-nets=( "aracne" "c3net" "genie3" "lassoAIC" "lassoBIC" "lassoCV1se" "mrnet" "ridgeAIC" "ridgeBIC" "ridgeCV1se" "ridgeCVmin" "sparrowZ" "sparrow2Z" "tigress" "wgcnaSoftThreshold" "wgcnaTopologicalOverlapMatrix" )
+nets=( "aracne" "c3net" "genie3" "lassoAIC" "lassoBIC" "lassoCV1se" "lassoCVmin" "mrnet" "ridgeAIC" "ridgeBIC" "ridgeCV1se" "ridgeCVmin" "sparrowZ" "sparrow2Z" "tigress" "wgcnaSoftThreshold" "wgcnaTopologicalOverlapMatrix" )
 
 for net in ${nets[@]}; do
     if [ -e $outputpath/${net}Network.csv ]; then
--- testNetworkRegressionSubmission.sh
+++ testNetworkRegressionSubmission.sh
@@ -8,10 +8,10 @@ pathv="/shared/metanetworkSynapse/"
 . $pathv/config.sh
 
 #number of cores to reserve for job
-nthreads=16
+nthreads=32
 
-#number of cores to reserve for sparrow jobs (computationally intensive)
-nthreadsSparrow=64
+#number of cores to reserve for computationally intensive jobs
+nthreadsHeavy=96
 
 #path to csv file with annotations to add to file on Synapse
 annotationFile="$outputpath/annoFile.txt"
@@ -21,15 +21,17 @@ provenanceFile="$outputpath/provenanceFile.txt"
 
 ### build nets ###
 
-qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreadsSparrow -S /bin/bash -V -cwd -N "sparrowZ" -e "$errorOutput/sparrowZerror.txt" -o "$outOutput/sparrowZout.txt" $pathv/networkScripts/sparrowZ.sh
+qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N "sparrowZ" -e "$errorOutput/sparrowZerror.txt" -o "$outOutput/sparrowZout.txt" $pathv/networkScripts/sparrowZ.sh
 
-qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreadsSparrow -S /bin/bash -V -cwd -N "sparrow2Z" -e "$errorOutput/sparrow2Zerror.txt" -o "$outOutput/sparrow2Zout.txt" $pathv/networkScripts/sparrow2Z.sh
+qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N "sparrow2Z" -e "$errorOutput/sparrow2Zerror.txt" -o "$outOutput/sparrow2Zout.txt" $pathv/networkScripts/sparrow2Z.sh
 
 qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N "lassoAIC" -e "$errorOutput/lassoAICerror.txt" -o "$outOutput/lassoAICout.txt" $pathv/networkScripts/lassoAIC.sh
 
 qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N  "lassoBIC" -e "$errorOutput/lassoBICerror.txt" -o "$outOutput/lassoBICout.txt" $pathv/networkScripts/lassoBIC.sh
 
 qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N  "lassoCV1se" -e "$errorOutput/lassoCV1seerror.txt" -o "$outOutput/lassoCV1seout.txt" $pathv/networkScripts/lassoCV1se.sh
+
+qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N  "lassoCVmin" -e "$errorOutput/lassoCVminerror.txt" -o "$outOutput/lassoCVminout.txt" $pathv/networkScripts/lassoCVmin.sh
 
 qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N  "ridgeAIC" -e "$errorOutput/ridgeAICerror.txt" -o "$outOutput/ridgeAICout.txt" $pathv/networkScripts/ridgeAIC.sh
 
@@ -39,6 +41,6 @@ qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outpu
 
 qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N  "ridgeCVmin" -e "$errorOutput/ridgeCVminerror.txt" -o "$outOutput/ridgeCVminout.txt" $pathv/networkScripts/ridgeCVmin.sh
 
-qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N  "genie3" -e "$errorOutput/genie3error.txt" -o "$outOutput/genie3out.txt" $pathv/networkScripts/genie3.sh
+qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreadsHeavy,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreadsHeavy -S /bin/bash -V -cwd -N  "genie3" -e "$errorOutput/genie3error.txt" -o "$outOutput/genie3out.txt" $pathv/networkScripts/genie3.sh
 
-qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreads,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreads -S /bin/bash -V -cwd -N  "tigress" -e "$errorOutput/tigresserror.txt" -o "$outOutput/tigressout.txt" $pathv/networkScripts/tigress.sh
+qsub -r yes -v s3=$s3,dataFile=$dataFile,pathv=$pathv,numberCore=$nthreadsHeavy,outputpath=$outputpath,s3b=$s3b,parentId=$parentId,annotationFile=$annotationFile,provenanceFile=$provenanceFile -pe mpi $nthreadsHeavy -S /bin/bash -V -cwd -N  "tigress" -e "$errorOutput/tigresserror.txt" -o "$outOutput/tigressout.txt" $pathv/networkScripts/tigress.sh

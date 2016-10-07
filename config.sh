edd7db268abd966015a1705198cc68b14a0381be
Phil Snyder <phil.snyder@sagebase.org>
2016-10-07 21:16:25 +0000

    correct config

--- config.sh
+++ config.sh
@@ -4,13 +4,13 @@
 outputpath="/shared/network/"
 
 # location of Expression data on Synapse
-dataSynId="syn1234567"
+dataSynId="syn7342897"
 
 #location of data file on master node
 dataFile="$outputpath/Expression.csv"
 
 #id of folder on Synapse that network files will go to
-parentId="syn7654321"
+parentId="syn7342900"
 
 #path to error output
 errorOutput="$outputpath/errorLogs"

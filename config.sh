787796b618ab9f3808bf9469c43d6058e0ef6baf
Ben Logsdon <ben.logsdon@sagebase.org>
2017-02-13 21:24:45 -0800

    mayo submission

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@
 outputpath="/shared/network/"
 
 # location of Expression data on Synapse
-dataSynId="syn7984097"
+dataSynId="syn8257627"
 
 # id of folder on Synapse that network files will go to
-parentId="syn7984378"
+parentId="syn8257430"
 
 # path to error output
 errorOutput="$outputpath/errorLogs"
@@ -16,4 +16,4 @@ errorOutput="$outputpath/errorLogs"
 outOutput="$outputpath/outLogs"
 
 # commit message
-versionCommitMessage="ROSMAP fix provenance"
+versionCommitMessage="Mayo TCX"

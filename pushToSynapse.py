2697831db400e474f06ce9abeffea3f083709604
Ben Logsdon <ben.logsdon@sagebase.org>
2017-02-13 16:36:27 -0800

    test of new provenance generation script using Kenny's code

--- pushToSynapse.py
+++ pushToSynapse.py
@@ -2,6 +2,67 @@ import synapseclient
 import argparse
 import github
 import csv
+import os
+import ConfigParser
+
+"""Get permalink to git commit for a specific path.
+written by Kenneth Daily (@kdaily)
+"""
+def getToken():
+    """Authenticate using a token.
+
+    """
+
+    config = ConfigParser.ConfigParser()
+    config.readfp(file(os.path.expanduser('~/.pygit')))
+
+    mytoken = config.get('authentication', 'token')
+
+    return mytoken
+
+_base_raw = "https://raw.github.com"
+_base_web = "https://github.com"
+
+def makeRawPermalink(repo, commit, path):
+    permalink = "%(base)s/%(repo)s/%(commit)s/%(path)s" % dict(base=_base_raw,
+                                                               repo=repo,
+                                                               commit=commit,
+                                                               path=path)
+    return permalink
+
+def makeWebPermalink(repo, commit, path):
+    permalink = "%(base)s/%(repo)s/blob/%(commit)s/%(path)s" % dict(base=_base_web,
+                                                                    repo=repo,
+                                                                    commit=commit,
+                                                                    path=path)
+    return permalink
+
+
+def getPermalink(gh, repo, path, ref="master", raw=True):
+    ## Get the repo
+    repoobj = gh.get_repo(repo)
+
+    ## Get a specific ref
+    refobj = repoobj.get_git_ref("heads/%s" % ref)
+
+    ## Get the commit object for this ref
+    commit = repoobj.get_commit(refobj.object.sha)
+
+    ## Get tree
+    tree = repoobj.get_git_tree(commit.sha, recursive=True)
+
+    try:
+        myfile = filter(lambda x: x.path == path, tree.tree)[0]
+    except IndexError as e:
+        raise(IndexError, "Not Found!")
+
+    if raw:
+        permalink = makeRawPermalink(repo=repo, commit=refobj.object.sha, path=path)
+    else:
+        permalink = makeWebPermalink(repo=repo, commit=refobj.object.sha, path=path)
+
+    return permalink
+
 
 def read_args():
     parser = argparse.ArgumentParser(description='push files to synapse')
@@ -28,28 +89,37 @@ def push(filePath, parentId, annotationFile, provenanceFile, method, branch, tok
         used = [r['provenance'] for r in reader if r['executed'] == 'FALSE']
         executed= [r['provenance'] for r in reader if r['executed'] == 'TRUE']
         g = github.Github(token)
-	repo = g.get_repo("Sage-Bionetworks/metanetworkSynapse")
-	config = repo.get_contents("config.sh", ref=branch)
-	thisScript = repo.get_contents("pushToSynapse.py", ref=branch)
-	networkScriptName = get_ns_name(method)
-        networkScript = repo.get_contents("networkScripts/%s.sh" % networkScriptName,
-	    ref=branch)
+	    #repo = g.get_repo("Sage-Bionetworks/metanetworkSynapse")
+	    #config = repo.get_contents("config.sh", ref=branch)
+        config = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="config.sh")
+	    #thisScript = repo.get_contents("pushToSynapse.py", ref=branch)
+        thisScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="pushToSynapse.py",raw=False)
+        networkScriptName = get_ns_name(method)
+        #networkScript = repo.get_contents("networkScripts/%s.sh" % networkScriptName,
+	    #ref=branch)
+        networkScript = getPermalink(gh=g,repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="networkScripts/%s.sh" % networkScriptName, raw=False)
         if method == "rankConsensus" or method == "bic":
-	    q = syn.chunkedQuery("select id, name from entity \
-		where entity.parentId=='%s'" % parentId)
+	        q = syn.chunkedQuery("select id, name from entity where entity.parentId=='%s'" % parentId)
             [used.append(i['entity.id']) for i in q]
-	    submissionScript = repo.get_contents("submissionConsensus.sh", ref=branch)
-            pushScript = repo.get_contents("pushConsensus.sh", ref=branch)
-            buildScript = repo.get_contents("buildConsensus.R", ref=branch)
+	    #submissionScript = repo.get_contents("submissionConsensus.sh", ref=branch)
+            submissionScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="submissionConsensus.sh",raw=False)
+            #pushScript = repo.get_contents("pushConsensus.sh", ref=branch)
+            pushScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="pushConsensus.sh",raw=False)
+            #buildScript = repo.get_contents("buildConsensus.R", ref=branch)
+            buildScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="buildConsensus.R",raw=False)
         else:
-	    submissionScript = repo.get_contents("submission.sh", ref=branch)
-	    pushScript = repo.get_contents("pushNetworks.sh", ref=branch)
+	        #submissionScript = repo.get_contents("submission.sh", ref=branch)
+            submissionScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="submission.sh",raw=False)
+	        #pushScript = repo.get_contents("pushNetworks.sh", ref=branch)
+            pushScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="pushNetworks.sh",raw=False)
 	    if networkScriptName in ['c3net', 'wgcnaTOM', 'mrnet']:
-	        buildScript = repo.get_contents("buildOtherNet.R")
+	        #buildScript = repo.get_contents("buildOtherNet.R")
+            buildScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="buildOtherNet.R",raw=False)
 	    else:
-		buildScript = repo.get_contents("buildMpiNet.R")
-    executed += [config.html_url, networkScript.html_url, buildScript.html_url,
-	submissionScript.html_url, pushScript.html_url, thisScript.html_url]
+		    #buildScript = repo.get_contents("buildMpiNet.R")
+            buildScript = getPermalink(gh=g, repo="Sage-Bionetworks/metanetworkSynapse", ref="master", path="buildMpiNet.R",raw=False)
+    executed += [config, networkScript, buildScript,
+	submissionScript, pushScript, thisScript]
     activity = synapseclient.Activity(name='Network Inference',
             description=method, used=used, executed=executed)
     synFile = synapseclient.File(filePath, parent=parentId)

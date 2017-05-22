9b5e2d68c2a89eb33318aea2a37c5b5b09e180fa
Thanneer Malai Perumal <thanneer.perumal@sagebase.org>
2017-05-22 19:09:05 +0000

    Minor modifications

--- buildModules.R
+++ buildModules.R
@@ -7,14 +7,14 @@ rankConsNet.id = commandArgs(TRUE)[[2]];#'syn8268680'
 
 module.method = commandArgs(TRUE)[[3]];#'fast_greedy'
 path = commandArgs(TRUE)[[4]];#'/shared/Github/metanetwork/CFinder-2.0.6--1448/' or '/shared/Github/metanetwork/GANXiS_v3.0.2/'
+runId = commandArgs(TRUE)[[5]];#'1'
 
-repository = commandArgs(TRUE)[[5]];#'th1vairam/metanetworkSynapse'
-branchName = commandArgs(TRUE)[[6]];#'modules_dev'
-fileName = commandArgs(TRUE)[[7]];#'buildModules.R'  
+repository = commandArgs(TRUE)[[6]];#'th1vairam/metanetworkSynapse'
+branchName = commandArgs(TRUE)[[7]];#'modules_dev'
+fileName = commandArgs(TRUE)[[8]];#'buildModules.R'  
 
-# apiKey.file = commandArgs(TRUE)[[8]];#'/shared/apikey.txt' 
-configPath = commandArgs(TRUE)[[8]];#'/home/rstudio/synapseConfig'
-library.path = commandArgs(TRUE)[[9]];#'/mnt/mylibs'
+configPath = commandArgs(TRUE)[[9]];#'/home/rstudio/synapseConfig'
+library.path = commandArgs(TRUE)[[10]];#'/mnt/mylibs'
 
 #### Set library paths ####
 .libPaths(library.path)
@@ -72,26 +72,17 @@ rm(list = c('bicNetworks', 'rank.cons'))
 gc()
 
 #### Compute modules using specified algorithm ####
-# Get a specific algorithm
-findModules.algo = switch (module.method,
-                           CFinder = metanetwork::findModules.CFinder,
-                           GANXiS = metanetwork::findModules.GANXiS,
-                           fast_greedy = metanetwork::findModules.fast_greedy,
-                           label_prop = metanetwork::findModules.label_prop, 
-                           louvain = metanetwork::findModules.louvain,
-                           spinglass = metanetwork::findModules.spinglass,
-                           walktrap = metanetwork::findModules.walktrap,
-                           infomap = metanetwork::findModules.infomap, 
-                           linkcommunities = metanetwork::findModules.linkcommunities)
-
 # Compute modules
-if (module.method == 'CFinder'){
-  mod = findModules.algo(adj, path = path, nperm = 100, min.module.size = 30)
-} else if (module.method == 'GANXiS'){
-  mod = findModules.algo(adj, path = path, nperm = 100, min.module.size = 30)
-} else{
-  mod <- findModules.algo(adj, nperm = 100, min.module.size = 30)
-}
+mod = switch (module.method,
+              CFinder = metanetwork::findModules.CFinder(adj, path = path, nperm = 10, min.module.size = 30),
+              GANXiS = metanetwork::findModules.GANXiS(adj, path = path, nperm = 10, min.module.size = 30),
+              fast_greedy = metanetwork::findModules.fast_greedy(adj, nperm = 10, min.module.size = 30),
+              label_prop = metanetwork::findModules.label_prop(adj, nperm = 10, min.module.size = 30), 
+              louvain = metanetwork::findModules.louvain(adj, nperm = 10, min.module.size = 30),
+              spinglass = metanetwork::findModules.spinglass(adj, nperm = 10, min.module.size = 30),
+              walktrap = metanetwork::findModules.walktrap(adj, nperm = 10, min.module.size = 30),
+              infomap = metanetwork::findModules.infomap(adj, nperm = 10, min.module.size = 30), 
+              linkcommunities = metanetwork::findModules.linkcommunities(adj, nperm = 10, min.module.size = 30))
 
 # Find modularity quality metrics
 mod = as.data.frame(mod)
@@ -109,8 +100,8 @@ fold = synStore(fold)
 
 # Write results to synapse
 system(paste('mkdir',bicNet.id))
-write.table(mod, file = paste0(bicNet.id,'/',module.method,'.modules.tsv'), row.names=F, quote=F, sep = '\t')
-obj = synapseClient::File(paste0(bicNet.id,'/',module.method,'.modules.tsv'), parentId = fold$properties$id)
+write.table(mod, file = paste0(bicNet.id,'/',module.method,'.',runId,'.modules.tsv'), row.names=F, quote=F, sep = '\t')
+obj = synapseClient::File(paste0(bicNet.id,'/',module.method,'.',runId,'.modules.tsv'), parentId = fold$properties$id)
 synapseClient::annotations(obj) = synapseClient::annotations(bic.obj)
 obj$annotations$fileType = "tsv"
 obj$annotations$analysisType = "moduleIdentification"
@@ -120,8 +111,8 @@ obj$annotations$NQ = NQ
 obj$annotations$Qds = Qds
 obj = synapseClient::synStore(obj, used = all.used.ids, executed = thisFile, activityName = 'Module Identification')
 
-write.table(module.qc.metrics, file = paste0(bicNet.id,'/',module.method,'.moduleQCMetrics.tsv'), row.names=F, quote=F, sep = '\t')
-obj.qc = synapseClient::File(paste0(bicNet.id,'/',module.method,'.moduleQCMetrics.tsv'), parentId = fold$properties$id)
+write.table(module.qc.metrics, file = paste0(bicNet.id,'/',module.method,'.',runId,'.moduleQCMetrics.tsv'), row.names=F, quote=F, sep = '\t')
+obj.qc = synapseClient::File(paste0(bicNet.id,'/',module.method,'.',runId,'.moduleQCMetrics.tsv'), parentId = fold$properties$id)
 synapseClient::annotations(obj.qc) = synapseClient::annotations(obj)
 obj.qc$annotations$analysisType = "moduleQC"
 obj.qc = synapseClient::synStore(obj.qc, activity = synGetActivity(obj))
